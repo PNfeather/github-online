68bbb1e48be280ec664dfca3cebff5fcea8f3288
PNfeather <921192114@qq.com>
2025-05-22 19:48:15 +0800

    msg

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 import postCssPxToRem from 'postcss-pxtorem'
@@ -13,7 +12,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(), // 自动导入的插件，解析器可以是 vant element and-vue
     Components({
       dts: false,
       // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
