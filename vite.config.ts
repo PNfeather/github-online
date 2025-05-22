777b5c1c442987593fbb088d1ed5ad909c9c6d5f
PNfeather <921192114@qq.com>
2025-05-22 19:31:45 +0800

    初始化

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,43 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import postCssPxToRem from 'postcss-pxtorem'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    vueDevTools(), // 自动导入的插件，解析器可以是 vant element and-vue
+    Components({
+      dts: false,
+      // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
+      resolvers: [VantResolver({ importStyle: false })],
+    }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 75, // 1rem的大小
+          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+          selectorBlackList: ['.van'], // 忽略转换正则匹配项
+          minPixelValue: 2, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
+          exclude: [/node_modules/], // 忽略转换正则匹配项
+        }),
+      ],
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    port: 8088, //端口号
+    host: '0.0.0.0', //ip地址 或 '0.0.0.0' 或 "loaclhost"
+    open: true, //启动后是否自动打开浏览器
   },
 })
