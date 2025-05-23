3f1e761d0b5a26a2aa32b432c1231326409cc247
PNfeather <921192114@qq.com>
2025-05-23 10:58:40 +0800

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
@@ -10,38 +10,43 @@ import postCssPxToRem from 'postcss-pxtorem'
 console.log(process.env.NODE_ENV)
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '/' : '/github-online/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    Components({
-      dts: false,
-      // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
-      resolvers: [VantResolver({ importStyle: false })],
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        postCssPxToRem({
-          rootValue: 37.5, // 1rem的大小
-          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
-          selectorBlackList: ['.van'], // 忽略转换正则匹配项
-          minPixelValue: 2, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
-          exclude: /node_modules/, // 忽略转换正则匹配项
-        }),
-      ],
+export default defineConfig(({ mode }) => {
+  // 获取环境变量,通过 loadEnv 方式
+  const env = loadEnv(mode, process.cwd())
+  const BASE_URL = env.VITE_BASE_URL
+  return {
+    base: BASE_URL || '/',
+    plugins: [
+      vue(),
+      vueJsx(),
+      Components({
+        dts: false,
+        // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
+        resolvers: [VantResolver({ importStyle: false })],
+      }),
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          postCssPxToRem({
+            rootValue: 37.5, // 1rem的大小
+            propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+            selectorBlackList: ['.van'], // 忽略转换正则匹配项
+            minPixelValue: 2, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
+            exclude: /node_modules/, // 忽略转换正则匹配项
+          }),
+        ],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 8088, //端口号
-    host: '0.0.0.0', //ip地址 或 '0.0.0.0' 或 "loaclhost"
-    open: true, //启动后是否自动打开浏览器
-  },
+    server: {
+      port: 8088, //端口号
+      host: '0.0.0.0', //ip地址 或 '0.0.0.0' 或 "loaclhost"
+      open: true, //启动后是否自动打开浏览器
+    },
+  }
 })
